b4772a2c9708cde42e54fbb1f75bf452e6230083
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-04-13 11:47:29 -0400

    Resolved #26.

--- main.tf
+++ main.tf
@@ -14,11 +14,11 @@ terraform {
   required_providers {
         confluent = {
             source  = "confluentinc/confluent"
-            version = "~> 2.19.0"
+            version = "~> 2.24.0"
         }
         aws = {
             source  = "hashicorp/aws"
-            version = "~> 5.90.0"
+            version = "~> 5.94.1"
         }
     }
 }
